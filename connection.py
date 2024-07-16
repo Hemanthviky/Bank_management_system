134536392380a5fa763bb3a08e82b9d5ca18bbc2
Hemanth Narayanan <96944253+Hemanthviky@users.noreply.github.com>
2024-07-16 09:47:59 +0530

    Add files via upload

---
+++ connection.py
@@ -0,0 +1,12 @@
+import pyodbc
+pyodbc.drivers()
+
+class Conn:
+    connection = pyodbc.connect('DRIVER={MySQL ODBC 8.3 Unicode Driver};'
+                                'SERVER=127.0.0.1;'
+                                'PORT=3306;'
+                                'DATABASE=bank;'
+                                'USER=root;'
+                                'PASSWORD=Vikashini@7;'
+                                'TRUSTED_CONNECTION=Yes;')
+    cursor = connection.cursor()
